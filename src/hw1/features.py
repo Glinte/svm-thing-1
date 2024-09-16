5fe45eef784bc9b1ceadd53708a695c93ed766fb
Glinte <96855131+Glinte@users.noreply.github.com>
2024-09-16 15:40:36 +0800

    Add pickle_path to detect_edges_canny for caching

--- src/hw1/features.py
+++ src/hw1/features.py
@@ -8,7 +8,7 @@ import numpy as np
 from skimage import feature
 import torch
 
-from hw1.main import train_data
+from hw1.main import train_data, test_data
 from hw1.visualization import visualize_grayscale_image, visualize_rgb_image, visualize_images
 
 if TYPE_CHECKING:
@@ -64,6 +64,7 @@ def detect_edges_canny[Batch: int, Height: int, Width: int](
     sigma: float = 0.75,
     low_threshold: int = 80,
     high_threshold: int = 160,
+    pickle_path: str | None = None,
 ) -> np.ndarray[tuple[Batch, Height, Width], np.dtype[np.bool_]]:
     """Detect edges in an image using the Canny edge detector.
 
@@ -72,6 +73,7 @@ def detect_edges_canny[Batch: int, Height: int, Width: int](
         sigma (float): Standard deviation of the Gaussian filter
         low_threshold (int): Lower threshold for the hysteresis procedure
         high_threshold (int): Higher threshold for the hysteresis procedure
+        pickle_path (str): Path to save the edge map to
 
     Returns:
         Edge map tensor of shape (H, W)
@@ -81,6 +83,9 @@ def detect_edges_canny[Batch: int, Height: int, Width: int](
     edges: np.ndarray[tuple[Batch, Height, Width], np.dtype[np.bool_]] = np.zeros_like(image_gray, dtype=bool)
     for n in range(image_rgb.shape[0]):
         edges[n] = feature.canny(image_gray[n], sigma, low_threshold, high_threshold)
+
+    if pickle_path:
+        np.save(pickle_path, edges)
     return edges
 
 
@@ -89,15 +94,15 @@ def main():
     from timeit import default_timer as timer
     logging.basicConfig(level=logging.INFO)
 
-    N_SAMPLES = 100
+    N_SAMPLES = 10000
 
     time_start = timer()
     edges = detect_edges_canny(train_data[:N_SAMPLES].reshape(N_SAMPLES, 3, 32, 32))
     time_end = timer()
     logger.info(f"Time taken to detect edges: {time_end - time_start:.5f} seconds")
 
-    visualize_images(edges)
-    visualize_images(train_data[:100])
+    # visualize_images(edges)
+    # visualize_images(train_data[:100])
 
 
 if __name__ == "__main__":
