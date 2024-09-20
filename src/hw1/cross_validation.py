49c9c7947aafc569bb84a3b36aedbbeb1c4619c3
Glinte <96855131+Glinte@users.noreply.github.com>
2024-09-20 10:51:20 +0800

    Move find_best_k_with_cross_validation to cross_validation.py

---
+++ src/hw1/cross_validation.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from typing import Any
+
+import numpy as np
+from numpy import typing as npt
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+from hw1.knn import AnnoyClassifier
+from hw1.main import logger
+
+
+def find_best_k_with_cross_validation(
+    X: npt.ArrayLike,
+    y: npt.ArrayLike,
+    classifier_cls: type[KNeighborsClassifier] | type[AnnoyClassifier] | None = None,
+    init_kwargs: dict[str, Any] | None = None,
+    n_splits: int = 5,
+) -> int:
+    """Find the best k for the K-Nearest Neighbors algorithm using cross-validation.
+
+    Args:
+        X: The data.
+        y: The labels.
+        classifier_cls: The classifier class.
+        init_kwargs: The initialization keyword arguments.
+        n_splits: The number of splits. This is the number of times the data is split into training and testing sets to do cross-validation.
+    """
+    if classifier_cls is None:
+        raise ValueError("classifier_cls must be provided.")
+
+    best_k = 0
+    best_score = 0
+    for k in range(1, 10):
+        scores = []
+
+        for i in range(n_splits):
+            X_train, X_test, y_train, y_test = train_test_split(
+                X, y, test_size=0.05, train_size=0.9, shuffle=True, random_state=42 + i
+            )  # Fixed random state for reproducibility and caching
+            if issubclass(classifier_cls, (KNeighborsClassifier, AnnoyClassifier)):
+                classifier = classifier_cls(n_neighbors=k, **(init_kwargs or {}))
+            else:
+                raise ValueError("Invalid classifier.")
+            classifier.fit(X_train, y_train)
+            y_pred = classifier.predict(X_test)
+            score = metrics.f1_score(y_test, y_pred, average="macro")
+            logger.info(f"k: {k}, test: {i+1}, F1 score: {score}")
+            scores.append(score)
+
+        avg_score = np.mean(scores)
+        print(f"k: {k}, F1 score: {avg_score}")
+        if avg_score > best_score:
+            best_score = avg_score
+            best_k = k
+    print(f"Best k: {best_k}, F1 score: {best_score}")
+    return best_k
