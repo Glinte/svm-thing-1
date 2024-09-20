e169700f9670dabeb51cbf21c8818e4712bc04d7
Glinte <96855131+Glinte@users.noreply.github.com>
2024-09-20 12:44:55 +0800

    Implement detect_corners_harris

--- src/hw1/features.py
+++ src/hw1/features.py
@@ -7,9 +7,11 @@ from typing import TYPE_CHECKING, Any, overload, Literal
 
 import numpy as np
 from skimage import feature
+import cv2 as cv
 import torch
 
 from hw1 import train_data
+from hw1.visualization import visualize_images
 
 if TYPE_CHECKING:
     pass
@@ -65,7 +67,7 @@ def rgb_to_grayscale(image_rgb):
 
 def detect_edges_canny[Batch: int, Height: int, Width: int](
     image_rgb: np.ndarray[
-        tuple[Batch, Literal[3], Height, Width], np.dtype[np.number[Any]]
+        tuple[Batch, Literal[3], Height, Width], np.dtype[np.float64]
     ],
     *,
     sigma: float = 0.75,
@@ -98,6 +100,43 @@ def detect_edges_canny[Batch: int, Height: int, Width: int](
     return edges
 
 
+def detect_corners_harris[Batch: int, Height: int, Width: int](
+    image_rgb: np.ndarray[
+        tuple[Batch, Literal[3], Height, Width], np.dtype[np.float64]
+    ],
+    *,
+    method: str = 'k',
+    k: float = 0.04,
+    eps: float = 0.000001,
+    sigma: int = 1,
+    pickle_path: str | None = None,
+) -> np.ndarray[tuple[Batch, Height, Width], np.dtype[np.float64]]:
+    """Detect corners in an image using the Harris corner detector.
+
+    Args:
+        image_rgb: Input RGB image tensor of shape (Batch, 3, H, W)
+        method: Method to use for the Harris corner detector
+        k: Harris detector free parameter
+        eps: Harris detector free parameter
+        sigma: Standard deviation of the Gaussian filter
+        pickle_path: Path to save the corner map to
+
+    Returns:
+        Corner map tensor of shape (Batch, H, W)
+    """
+    image_gray = rgb_to_grayscale(image_rgb)
+
+    corners: np.ndarray[tuple[Batch, Height, Width], np.dtype[np.float64]] = np.zeros_like(image_gray, dtype=np.float64)
+    for n in range(image_rgb.shape[0]):
+        # The corner_peaks function returns the coordinates of the corner peaks
+        peaks = feature.corner_peaks(feature.corner_harris(image_gray[n], method=method, k=k, eps=eps, sigma=sigma))
+        corners[n][peaks[:, 0], peaks[:, 1]] = 1
+
+    if pickle_path:
+        np.save(pickle_path, corners)
+    return corners
+
+
 def main():
     """Quick testing, not part of the library."""
     from timeit import default_timer as timer
@@ -107,12 +146,18 @@ def main():
     N_SAMPLES = 50000
 
     time_start = timer()
-    edges = detect_edges_canny(train_data[:N_SAMPLES].reshape(N_SAMPLES, 3, 32, 32), pickle_path="train_data_edges.npy")
+    corners = detect_corners_harris(train_data[:N_SAMPLES].reshape(N_SAMPLES, 3, 32, 32), pickle_path="train_data_edges.npy")
     time_end = timer()
     logger.info(f"Time taken to detect edges: {time_end - time_start:.5f} seconds")
+    # corners = np.load("train_data_edges.npy")
 
-    # visualize_images(edges)
-    # visualize_images(train_data[:100])
+    image_corners = visualize_images(corners[:100].astype(np.bool_), show=False)
+    image_originals = visualize_images(train_data[:100], show=False)
+    image_corners.putalpha(100)
+    image_originals.putalpha(180)
+    image_originals.paste(image_corners, (0, 0), image_corners)
+    image_originals.show()
+
 
 
 if __name__ == "__main__":
