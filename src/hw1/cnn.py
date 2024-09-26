e506f73becc0cb673423090f95b5fe8442facc7e
Glinte <96855131+Glinte@users.noreply.github.com>
2024-09-26 16:18:51 +0800

    Parameterize CNN with channels

--- src/hw1/cnn.py
+++ src/hw1/cnn.py
@@ -14,11 +14,16 @@ logger = logging.getLogger(__name__)
 
 
 class CNN(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, channels: int) -> None:
+        """Initialize the CNN.
+
+        Args:
+            channels: Number of channels in the input images.
+        """
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(channels, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv2 = nn.Conv2d(channels * 2, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
